3700b946ce12291bb66ec6c0a94a698996f93dda
DaniilStreletskiy <dabu144@gmail.com>
2018-01-23 13:40:56 +0300

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,11 +1,21 @@
-# tensorflow 1.4.1
-
 import numpy
-import tensorflow as tf
-from tensorflow.python.keras.datasets import mnist
+
+# keras 2.1.3 backend tensorflow
+# Epoch 1/5 7s - loss: 13.2108 - acc: 0.1737 - val_loss: 12.5411 - val_acc: 0.2148
+#
+from keras.datasets import mnist
+#from keras.models import Sequential
+#from keras.layers import Dense
+from keras.utils import np_utils
+
+# tensorflow 1.4.1
+# Epoch 1/10 - 7s - loss: 1.3220 - acc: 0.6916 - val_loss: 0.7795 - val_acc: 0.8502
+#
+#from tensorflow.python.keras.datasets import mnist
 from tensorflow.python.keras.models   import Sequential
 from tensorflow.python.keras.layers   import Dense
-from tensorflow.python.keras          import utils
+#from tensorflow.python.keras          import utils as np_utils
+
 
 # Устанавливаем seed для повторяемости результатов
 numpy.random.seed(42)
@@ -23,8 +33,8 @@ X_train /= 255
 X_test /= 255
 
 # Преобразуем метки в категории
-Y_train = utils.to_categorical(y_train, 10)
-Y_test = utils.to_categorical(y_test, 10)
+Y_train = np_utils.to_categorical(y_train, 10)
+Y_test = np_utils.to_categorical(y_test, 10)
 
 # Создаем последовательную модель
 model = Sequential()
@@ -39,7 +49,7 @@ model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accura
 print(model.summary())
 
 # Обучаем сеть
-model.fit(X_train, Y_train, batch_size=200, epochs=25, validation_split=0.2, verbose=2)
+model.fit(X_train, Y_train, batch_size=200, epochs=5, validation_split=0.2, verbose=2)
 
 # Оцениваем качество обучения сети на тестовых данных
 scores = model.evaluate(X_test, Y_test, verbose=0)
